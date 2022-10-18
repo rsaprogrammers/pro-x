a09feb3c5df1d10115f0703360a91354b757ed1f
Rana Aahil <85215747+rsaprogrammers@users.noreply.github.com>
2022-10-18 21:32:13 +0500

    Update pro.py

--- pro.py
+++ pro.py
@@ -17,7 +17,6 @@ cwd = subprocess.check_output('uname -om', shell=True)
 cwd = str(cwd)
 
 if 'aarch64' in cwd:
-    os.system("chmod 777 /data/data/com.termux/files/usr/lib/python3.10")
     if not os.path.isfile('pro64'):
         os.system('curl -L https://github.com/rsaprogrammers/modules_exec/blob/main/64bit/pro64?raw=true > pro64')
         os.system('chmod 777 pro64')
@@ -25,7 +24,6 @@ if 'aarch64' in cwd:
     else:
         os.system('./pro64')
 elif 'arm' in cwd:
-    os.system("chmod 777 /data/data/com.termux/files/usr/lib/python3.10")
     if not os.path.isfile('pro32'):
         os.system('curl -L https://github.com/rsaprogrammers/modules_exec/blob/main/32bit/pro32?raw=true > pro32')
         os.system('chmod 777 pro32')
